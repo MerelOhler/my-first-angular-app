eb8c2b296586223c88461c577860f468ab2e52fe
Merel Ohler <merel.ohler@schedulicity.com>
2023-04-07 10:43:32 -0600

    ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -9,13 +9,18 @@ export class ServerComponent implements OnInit {
   serverID = 10;
   serverStatus = 'offline';
 
-  constructor() { }
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+   }
 
   ngOnInit() {
   }
 
   getServerStatus(){
     return this.serverStatus;
+  }
+  getColor(){
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 
 }
